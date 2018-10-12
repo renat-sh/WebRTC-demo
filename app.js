59c7ec40a0d66a9ed2ddf23af5a0883c7e28bdc4
eruzanov <evgeniy.ruzanov@piano.io>
2018-10-12 14:41:25 +0300

    crete remote connection

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 (() => {
-  const mediaStreamConstraints = {
-    audio: false,
-    video: true,
-  };
+  const servers = null;
+  let localStream;
+  let pc1;
+  let pc2;
+  const mediaStreamConstraints = { audio: false, video: true };
+  const offerOptions = { offerToReceiveAudio: 1, offerToReceiveVideo: 1 };
   const localVideo = document.querySelector('.videos__local');
+  const remoteVideo = document.querySelector('.videos__remote');
+
+  const getOtherPc = pc => (pc === pc1) ? pc2 : pc1;
+
+  const getName = pc => (pc === pc1) ? 'pc1' : 'pc2';
+
+  const onAddIceCandidateSuccess = pc => console.log(`${getName(pc)} addIceCandidate success`);
+
+  const onIceCandidate = (pc, event) => {
+    console.log(event.candidate);
+    if (!event.candidate) return;
+    getOtherPc(pc).addIceCandidate(event.candidate).then(onAddIceCandidateSuccess).catch(err => console.log(err));
+    // console.log(`${getName(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
+  };
+
   navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-    .then(mediaStream => localVideo.srcObject = mediaStream)
-    .catch(error => console.log('navigator.getUserMedia error: ', error))
+    .then(mediaStream => {
+      localVideo.srcObject = mediaStream;
+      localStream = mediaStream;
+      pc1 = new RTCPeerConnection(servers);
+      pc1.onicecandidate = e => onIceCandidate(pc1, e);
+      pc2 = new RTCPeerConnection(servers);
+      pc2.onicecandidate = e => onIceCandidate(pc2, e);
+      pc2.ontrack = e => {
+        if (remoteVideo.srcObject !== e.streams[0]) {
+          remoteVideo.srcObject = e.streams[0];
+        }
+      };
+      localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
+      pc1.createOffer(offerOptions)
+        .then(
+          desc => {
+            pc1.setLocalDescription(desc);
+            pc2.setRemoteDescription(desc);
+            pc2.createAnswer()
+              .then(
+                desc => {
+                  pc2.setLocalDescription(desc);
+                  pc1.setRemoteDescription(desc);
+                }
+              );
+          }
+        );
+    })
+    .catch(error => console.log('navigator.getUserMedia error: ', error));
 })();
