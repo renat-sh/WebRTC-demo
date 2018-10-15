bddef503daff46225d57fc0dd4ed6a854caa7531
EugeneRuzanov <eugene.ruzanov@gmail.com>
2018-10-15 13:36:52 +0300

    - formated code with the prettier formatter

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (() => {
   const servers = {
     iceServers: [
-      {urls: 'stun:stun.l.google.com:19302'},
+      { urls: "stun:stun.l.google.com:19302" },
       {
-        urls: 'turn:test.medgreat.ru:3478',
-        username: 'username1',
-		    credential: 'password1',
+        urls: "turn:test.medgreat.ru:3478",
+        username: "username1",
+        credential: "password1"
       }
     ]
   };
@@ -14,16 +14,17 @@
   let pc2;
   const mediaStreamConstraints = { audio: false, video: true };
   const offerOptions = { offerToReceiveAudio: 1, offerToReceiveVideo: 1 };
-  const localVideo = document.querySelector('.videos__local');
-  const remoteVideo = document.querySelector('.videos__remote');
+  const localVideo = document.querySelector(".videos__local");
+  const remoteVideo = document.querySelector(".videos__remote");
 
   const onIceCandidate = (pc, event) => {
     if (!event.candidate) return;
-    console.log('onIceCandidate', event.candidate);
+    console.log("onIceCandidate", event.candidate);
     pc.addIceCandidate(event.candidate).catch(err => console.log(err));
   };
 
-  navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+  navigator.mediaDevices
+    .getUserMedia(mediaStreamConstraints)
     .then(mediaStream => {
       localVideo.srcObject = mediaStream;
       localStream = mediaStream;
@@ -37,21 +38,17 @@
           remoteVideo.srcObject = e.streams[0];
         }
       };
-      localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-      pc1.createOffer(offerOptions)
-        .then(
-          desc => {
-            pc1.setLocalDescription(desc);
-            pc2.setRemoteDescription(desc);
-            pc2.createAnswer()
-              .then(
-                desc => {
-                  pc2.setLocalDescription(desc);
-                  pc1.setRemoteDescription(desc);
-                }
-              );
-          }
-        );
+      localStream
+        .getTracks()
+        .forEach(track => pc1.addTrack(track, localStream));
+      pc1.createOffer(offerOptions).then(desc => {
+        pc1.setLocalDescription(desc);
+        pc2.setRemoteDescription(desc);
+        pc2.createAnswer().then(desc => {
+          pc2.setLocalDescription(desc);
+          pc1.setRemoteDescription(desc);
+        });
+      });
     })
-    .catch(error => console.log('navigator.getUserMedia error: ', error));
+    .catch(error => console.log("navigator.getUserMedia error: ", error));
 })();
